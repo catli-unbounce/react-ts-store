eb3eda9f5ff3a7bbb521466d0a1850ab0dd7dc0d
catli-unbounce <catherine.li@unbounce.com>
2022-07-31 18:56:11 -0700

    Initial commit

---
+++ src/pages/Store.tsx
@@ -0,0 +1,10 @@
+import storeItems from "../data/items.json";
+import { Col, Row } from "react-bootstrap";
+export function Store() {
+  return (
+    <>
+      <h1>Store</h1>
+      <Row></Row>
+    </>
+  );
+}
